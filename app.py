223efd0a43bf6e290575f48b8654aed5e8ac472e
Nicholas <87298104+NicholasTc@users.noreply.github.com>
2025-06-16 23:43:44 -0700

    Nicholas attempt and name added

--- app.py
+++ app.py
@@ -4,7 +4,20 @@ app = Flask(__name__)
 @app.route("/")
 
 def hello():
-    return "Hello World!"
+    # Nicholas' update: added functionality to print names (just fill in your names in the array list, it should avoid any merge conflict):
+    # !dont forget the comma 
+    names = [
+        "Nicholas is here!"
+    ]
+
+    items = "".join(f"<li>{name}</li>" for name in names);
+    return f"""
+        <h1>Hello World!</h1>
+        <ol>{items}</ol>
+    """
+
+    # commented original: cant have 2 returns
+    # return "Hello World!"
 
 if __name__ == "__main__":
     app.run()
